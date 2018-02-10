65d6aea11220e85a69455b6b74d19218e9d446d9
Gabriel Munteanu <gabimunteanu.sdt@gmail.com>
2018-02-10 22:06:15 +0100

    feat(Component) Code SpamReports component

--- src/app/components/spam-reports/spam-reports.component.ts
+++ src/app/components/spam-reports/spam-reports.component.ts
@@ -1,15 +1,72 @@
+import { NgRedux, select } from '@angular-redux/store';
 import { Component, OnInit } from '@angular/core';
+import { SpamReportsService } from './spam-reports.service';
+import { Observable } from 'rxjs/Observable';
+import { SpamReportsActions } from './spam-reports.actions';
+import { IAppState } from '../core/store/store.module';
+import { ISpamReport, SpamReports } from './spam-reports';
 
 @Component({
-  selector: 'app-spam-reports',
   templateUrl: './spam-reports.component.html',
-  styleUrls: ['./spam-reports.component.css']
+  selector: 'app-spam-reports',
+  styleUrls: ['./spam-reports.component.scss']
 })
 export class SpamReportsComponent implements OnInit {
 
-  constructor() { }
+  public spamReports: ISpamReport[];
+
+  @select(['data', 'spamReports'])
+  private _spamReports$: Observable<any>;
+
+  constructor(
+    private _spamReportsService: SpamReportsService,
+    private _spamReportsActions: SpamReportsActions,
+    private _ngRedux: NgRedux<IAppState>
+  ) {
+    this._spamReports$.subscribe((spamReports) => { this.spamReports = spamReports; });
+  }
+
+  public blockReport(spamReport): void {
+    this._spamReportsService.setSpamReportStatus(spamReport.id, 'BLOCKED')
+      .subscribe(
+        (result: any) => {
+          const spamReportEdited = new SpamReports(result);
+          this._spamReportsActions.saveSpamReport(spamReportEdited);
+        },
+        (error: any) => {
+          console.log(error);
+        }
+      );
+  }
+
+  public resolveReport(spamReport): void {
+    this._spamReportsService.setSpamReportStatus(spamReport.id, 'RESOLVED')
+      .subscribe(
+        (result: any) => {
+          const spamReportEdited = new SpamReports(result);
+          this._spamReportsActions.saveSpamReport(spamReportEdited);
+        },
+        (error: any) => {
+          console.log(error);
+        }
+      );
+  }
 
   ngOnInit() {
+    this._spamReportsService.getSpamReports()
+      .subscribe(
+        (result: any) => {
+          const spamReportsJSON = result;
+          const spamReports = spamReportsJSON.map((element) => {
+            return new SpamReports(element);
+          })
+          this._spamReportsActions.saveSpamReports(spamReports);
+          return;
+        },
+        (error: any) => {
+          console.error(error);
+        }
+      );
   }
 
 }
